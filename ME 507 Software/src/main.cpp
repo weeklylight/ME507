637dd88e43a3da071fc3381fd2232976e10866bf
adunn-kal <alexanderkdunn1@gmail.com>
2022-10-31 17:18:22 -0700

    Worked on code

---
+++ ME 507 Software/src/main.cpp
@@ -0,0 +1,110 @@
+#include <Arduino.h>
+#include "taskshare.h"
+#include "taskqueue.h"
+
+// Define period for tasks in mS
+#define IMU_DELAY 2
+#define MOTOR_DELAY 5
+#define GPS_DELAY 1000
+#define CALCULATION_DELAY 1000
+#define SEND_DELAY 2
+
+// Define shares and queues
+Share<bool> newData("New Data Flag");
+
+
+//----------------------Tasks----------------------------
+void task_imu(void* p_params)
+{
+  // Run continuosly
+  while (true)
+  {
+    // Do something
+
+    // Wait
+    vTaskDelay(IMU_DELAY);
+  }
+}
+
+void task_motor(void* p_params)
+{
+  // Run continuosly
+  while (true)
+  {
+    // Do something
+
+    // Wait
+    vTaskDelay(MOTOR_DELAY);
+  }
+}
+
+void task_gps(void* p_params)
+{
+  // Run continuosly
+  while (true)
+  {
+    Serial.println("GPS Task");
+
+    // Set new data flag
+    newData.put(true);
+
+    // Get gps data
+
+    // Wait
+    vTaskDelay(GPS_DELAY);
+  }
+}
+
+void task_send(void* p_params)
+{
+  // Run continuosly
+  while (true)
+  {
+    if (newData.get())
+    {
+      // Reset flag
+      newData.put(false);
+      Serial.println("Sending data");
+
+      // Send data
+    }
+
+    // Wait
+    vTaskDelay(SEND_DELAY);
+  }
+}
+
+void task_position(void* p_params)
+{
+  // Run continuosly
+  while (true)
+  {
+    // Run calculations
+
+    // Wait
+    vTaskDelay(CALCULATION_DELAY);
+  }
+}
+
+//-------------------------------------------------------
+void setup()
+{
+  Serial.begin(115200);
+  if (!Serial) {}
+
+  Serial.println("Starting");
+
+  xTaskCreate(task_imu, "IMU Position", 1024, NULL, 10, NULL);
+  xTaskCreate(task_motor, "Motor Controller", 1024, NULL, 8, NULL);
+  xTaskCreate(task_gps, "GPS Data", 1024, NULL, 6, NULL);
+  xTaskCreate(task_send, "Send Data", 1024, NULL, 4, NULL);
+  xTaskCreate(task_position, "Position Calculation", 1024, NULL, 2, NULL);
+  
+  Serial.println("Tasks Created");
+}
+
+void loop()
+{
+  // Do nothing
+}
+//-------------------------------------------------------
