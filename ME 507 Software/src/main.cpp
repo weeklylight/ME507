842fee861a4023aa8e679ee82c98190f042fc47e
adunn-kal <alexanderkdunn1@gmail.com>
2022-11-03 10:12:09 -0700

    Setup basic shares and queues

--- ME 507 Software/src/main.cpp
+++ ME 507 Software/src/main.cpp
@@ -1,6 +1,6 @@
 /**
  * @file main.cpp
- * @author your name (you@domain.com)
+ * @author Kevin and the Gang
  * @brief 
  * @version 0.1
  * @date 2022-11-03
@@ -9,28 +9,84 @@
  * 
  */
 
+//-----------------------Libraries------------------------
+
 #include <Arduino.h>
 #include "taskshare.h"
 #include "taskqueue.h"
 
-// Define period for tasks in mS
+//-------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+//------------------Task Periods [ms]---------------------
+
 #define IMU_DELAY 2
 #define MOTOR_DELAY 5
 #define GPS_DELAY 1000
 #define CALCULATION_DELAY 1000
 #define SEND_DELAY 2
 
-// Define shares and queues
+//-------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+//-------------------Shares & Queues----------------------
+
+// Send Data
 Share<bool> newData("New Data Flag");
+
+// GPS Data
+Queue<int16_t> satHeadings(100, "Satellite Headings");
+Share<float> latitude("Latitude");
+Share<float> longitude("Longitude");
+Share<float> altitude("Altitude");
+Share<uint8_t> siv("Satellites in View");
+
+// Target Data
+Share<float> targetX("Target Theta X");
+Share<float> targetY("Target Theta Y");
+
+// IMU Data
+Queue<float> thetaX(10, "Theta X");
+Queue<float> thetaY(10, "Theta Y");
+Queue<float> heading(10, "Heading");
+
+//-------------------------------------------------------
+
+
+
+
+
+
+
+
 
 
 //----------------------Tasks----------------------------
+
 void task_imu(void* p_params)
 {
-  // Run continuosly
   while (true)
   {
-    // Do something
+    // Get platform positons from IMU, put them in queues
+    thetaX.put(NULL);
+    thetaY.put(NULL);
+    heading.put(NULL);
 
     // Wait
     vTaskDelay(IMU_DELAY);
@@ -39,10 +95,10 @@ void task_imu(void* p_params)
 
 void task_motor(void* p_params)
 {
-  // Run continuosly
   while (true)
   {
-    // Do something
+    // Run motor controllers
+    // Get: thetaX, thetaY, heading, targetX, targetY
 
     // Wait
     vTaskDelay(MOTOR_DELAY);
@@ -51,15 +107,18 @@ void task_motor(void* p_params)
 
 void task_gps(void* p_params)
 {
-  // Run continuosly
   while (true)
   {
     Serial.println("GPS Task");
 
+    // Get gps data
+    latitude.put(NULL);
+    longitude.put(NULL);
+    altitude.put(NULL);
+    siv.put(NULL);
+
     // Set new data flag
     newData.put(true);
-
-    // Get gps data
 
     // Wait
     vTaskDelay(GPS_DELAY);
@@ -68,16 +127,18 @@ void task_gps(void* p_params)
 
 void task_send(void* p_params)
 {
-  // Run continuosly
   while (true)
   {
     if (newData.get())
     {
       // Reset flag
       newData.put(false);
-      Serial.println("Sending data");
+      
+      // Get data
+      // Get: latitude, longitude, altitude, SIV
 
       // Send data
+
     }
 
     // Wait
@@ -87,10 +148,16 @@ void task_send(void* p_params)
 
 void task_position(void* p_params)
 {
-  // Run continuosly
   while (true)
   {
+    // Get data
+    // Get: thetaX, thetaY, heading
+
     // Run calculations
+
+    // Update targets
+    targetX.put(0);
+    targetY.put(0);
 
     // Wait
     vTaskDelay(CALCULATION_DELAY);
@@ -98,13 +165,26 @@ void task_position(void* p_params)
 }
 
 //-------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+//---------------------Program---------------------------
+
 void setup()
 {
+  // Begin serial channel
   Serial.begin(115200);
   if (!Serial) {}
-
   Serial.println("Starting");
 
+  // Setup tasks
   xTaskCreate(task_imu, "IMU Position", 1024, NULL, 10, NULL);
   xTaskCreate(task_motor, "Motor Controller", 1024, NULL, 8, NULL);
   xTaskCreate(task_gps, "GPS Data", 1024, NULL, 6, NULL);
@@ -118,4 +198,5 @@ void loop()
 {
   // Do nothing
 }
+
 //-------------------------------------------------------
