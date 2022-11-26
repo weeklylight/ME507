a6159274bfde1c91015f1c6531b62d7065aa141d
weeklylight <56906650+weeklylight@users.noreply.github.com>
2022-11-25 23:43:07 -0800

    Revert "adding imu functionality to main"
    This reverts commit e5df8c1d7f87e2443ee7e29d55b520bb37085e25.

--- ME 507 Software/src/main.cpp
+++ ME 507 Software/src/main.cpp
@@ -1,6 +1,6 @@
 /**
  * @file main.cpp
- * @author Alex Dunn, Jakob Frabosilio, Kevin Lee
+ * @author Kevin and the Gang
  * @brief 
  * @version 0.1
  * @date 2022-11-03
@@ -14,10 +14,9 @@
 #include <Arduino.h>
 #include "taskshare.h"
 #include "taskqueue.h"
-#include <Adafruit_LSM6DSOX.h>
-#include <Adafruit_LIS3MDL.h>
+#include "Adafruit_LSM6DSOX.h"
+#include "Adafruit_LIS3MDL.h"
 #include "Adafruit_GPS.h"
-#include "imu.h"
 
 //-------------------------------------------------------
 
@@ -52,7 +51,6 @@
 // IMU Objects
 Adafruit_LSM6DSOX lsm6ds;
 Adafruit_LIS3MDL lis3mdl;
-Imu iamyou;
 
 // GPS Objects
 Adafruit_GPS GPS(&Serial2);
@@ -125,23 +123,19 @@ void task_imu(void* p_params)
 {
   while (true)
   {
-  // Get platform positons from IMU, put them in queues
-  sensors_event_t accel, gyro, mag, temp;
-  sensors_vec_t angles;
+    // Get platform positons from IMU, put them in queues
+    sensors_event_t accel, gyro, mag, temp;
 
-  /* Get new normalized sensor events */
-  lsm6ds.getEvent(&accel, &gyro, &temp);
-  lis3mdl.getEvent(&mag);
-  
-  angles = iamyou.get_angles(accel.acceleration);
-  Serial.printf("Angles: (%f, %f, %f) deg\n\n", angles.x, angles.y, angles.z);
+   /* Get new normalized sensor events */
+   lsm6ds.getEvent(&accel, &gyro, &temp);
+   lis3mdl.getEvent(&mag);
 
-  thetaX.put(angles.x);
-  thetaY.put(angles.y);
-  heading.put(NULL);
-  
-  // Wait
-  vTaskDelay(IMU_DELAY);
+    thetaX.put(NULL);
+    thetaY.put(NULL);
+    heading.put(NULL);
+
+    // Wait
+    vTaskDelay(IMU_DELAY);
   }
 }
 
@@ -151,9 +145,7 @@ void task_motor(void* p_params)
   {
     // Run motor controllers
     // Get: thetaX, thetaY, heading, targetX, targetY
-    //Serial.println(thetaX.get());
-    //ctrl = iamyou.get_motor_ctrl(angles, control_angles, 5);
-    
+
     // Wait
     vTaskDelay(MOTOR_DELAY);
   }
@@ -234,6 +226,7 @@ void task_position(void* p_params)
     vTaskDelay(CALCULATION_DELAY);
   }
 }
+
 //-------------------------------------------------------
 
 
@@ -258,25 +251,28 @@ void setup()
 
   Serial.println("Starting");
 
+  // Setup tasks
+  xTaskCreate(task_imu, "IMU Position", 1024, NULL, 10, NULL);
+  xTaskCreate(task_motor, "Motor Controller", 1024, NULL, 8, NULL);
+  xTaskCreate(task_gps, "GPS Data", 1024, NULL, 6, NULL);
+  xTaskCreate(task_send, "Send Data", 1024, NULL, 4, NULL);
+  xTaskCreate(task_position, "Position Calculation", 1024, NULL, 2, NULL);
+
+  Serial.println("Tasks created");
+
   // Setup for IMU
-  bool lsm6ds_success, lis3mdl_success;
-  // hardware I2C mode, can pass in address & alt Wire
-  lsm6ds_success = lsm6ds.begin_I2C(0x6A);
-  lis3mdl_success = lis3mdl.begin_I2C(0x1E);
-  Serial.println("LSM6DS and LIS3MDL Found!");
-  // lsm6ds.setAccelRange(LSM6DS_ACCEL_RANGE_2_G);
-  // lsm6ds.setAccelDataRate(LSM6DS_RATE_12_5_HZ);
-  // lsm6ds.setGyroRange(LSM6DS_GYRO_RANGE_250_DPS );
-  // lsm6ds.setGyroDataRate(LSM6DS_RATE_12_5_HZ);
-  lis3mdl.setDataRate(LIS3MDL_DATARATE_155_HZ);
-  lis3mdl.setRange(LIS3MDL_RANGE_4_GAUSS);
-  lis3mdl.setPerformanceMode(LIS3MDL_MEDIUMMODE);
-  lis3mdl.setOperationMode(LIS3MDL_CONTINUOUSMODE);
+  bool lsm6ds_success = lsm6ds.begin_I2C(0x6A);
+  bool lis3mdl_success = lis3mdl.begin_I2C(0x1E);
+  lis3mdl.setDataRate(LIS3MDL_DATARATE_155_HZ); // Set data rate
+  lis3mdl.setRange(LIS3MDL_RANGE_4_GAUSS); // Set range
+  lis3mdl.setPerformanceMode(LIS3MDL_MEDIUMMODE); // Set performance
+  lis3mdl.setOperationMode(LIS3MDL_CONTINUOUSMODE); // Set mode
   lis3mdl.setIntThreshold(500);
-  lis3mdl.configInterrupt(false, false, true, // enable z axis
-                          true, // polarity
-                          false, // don't latch
-                          true); // enabled!
+  lis3mdl.configInterrupt(false, false,
+                          true, // Enable z axis
+                          true, // Polarity
+                          false, // Don't latch
+                          true); // Enabled!
 
   Serial.println("IMU done");
   
@@ -285,15 +281,6 @@ void setup()
   GPS.sendCommand(PMTK_SET_NMEA_UPDATE_5HZ); // Set output rate
 
   Serial.println("GPS done");
-
-
-  // Setup tasks
-  xTaskCreate(task_imu, "IMU Position", 20480, NULL, 10, NULL);
-  xTaskCreate(task_motor, "Motor Controller", 1024, NULL, 8, NULL);
-  //xTaskCreate(task_gps, "GPS Data", 1024, NULL, 6, NULL);
-  //xTaskCreate(task_send, "Send Data", 1024, NULL, 4, NULL);
-  //xTaskCreate(task_position, "Position Calculation", 1024, NULL, 2, NULL);
-  Serial.println("Tasks created");
 }
 
 void loop()
